a1a8d558298044bbdc69d8055eb505852e5c4b29
suzhelan <suzhelan@gmail.com>
2025-02-01 21:10:26 +0800

    Initial commit

---
+++ app/src/main/java/top/sacz/hook/HookSteps.java
@@ -0,0 +1,75 @@
+package top.sacz.hook;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.ContextWrapper;
+import android.os.Bundle;
+
+import com.kongzue.dialogx.DialogX;
+import com.kongzue.dialogx.dialogs.PopTip;
+import com.kongzue.dialogxmaterialyou.style.MaterialYouStyle;
+
+import java.lang.reflect.Method;
+
+import de.robv.android.xposed.XC_MethodHook;
+import de.robv.android.xposed.XposedBridge;
+import de.robv.android.xposed.callbacks.XC_LoadPackage;
+import top.sacz.xphelper.reflect.MethodUtils;
+
+public class HookSteps {
+
+    /**
+     * 获取初始的Hook方法
+     *
+     * @param loadPackageParam
+     * @return
+     */
+    public Method getApplicationCreateMethod(XC_LoadPackage.LoadPackageParam loadPackageParam) {
+        try {
+            String applicationName = loadPackageParam.appInfo.name;
+            Class<?> clz = loadPackageParam.classLoader.loadClass(applicationName);
+            try {
+                return clz.getDeclaredMethod("attachBaseContext", Context.class);
+            } catch (Throwable i) {
+                try {
+                    return clz.getDeclaredMethod("onCreate");
+                } catch (Throwable e) {
+                    try {
+                        return clz.getSuperclass().getDeclaredMethod("attachBaseContext", Context.class);
+                    } catch (Throwable m) {
+                        return clz.getSuperclass().getDeclaredMethod("onCreate");
+                    }
+                }
+            }
+        } catch (Exception e) {
+            try {
+                return ContextWrapper.class.getDeclaredMethod("attachBaseContext", Context.class);
+            } catch (NoSuchMethodException ex) {
+                return null;
+            }
+        }
+    }
+
+    public void initHook(Context context) {
+        initDialogX(context);
+        //演示查找方法
+        Method activityCreateMethod = MethodUtils.create(Activity.class)
+                .returnType(void.class)
+                .params(Bundle.class)
+                .methodName("onCreate")
+                .first();
+        //展示注入成功的提示
+        XposedBridge.hookMethod(activityCreateMethod, new XC_MethodHook() {
+            @Override
+            protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                PopTip.show("注入成功 ->" + InjectHook.loadPackageParam.appInfo.name);
+            }
+        });
+    }
+
+    private void initDialogX(Context context) {
+        DialogX.init(context);
+        DialogX.globalTheme = DialogX.THEME.AUTO;
+        DialogX.globalStyle = new MaterialYouStyle();
+    }
+}
---
+++ xphelper/src/main/java/top/sacz/xphelper/XpHelper.java
@@ -0,0 +1,55 @@
+package top.sacz.xphelper;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+
+import de.robv.android.xposed.IXposedHookZygoteInit;
+import top.sacz.xphelper.activity.ActivityProxyManager;
+import top.sacz.xphelper.reflect.ClassUtils;
+import top.sacz.xphelper.util.ActivityTools;
+import top.sacz.xphelper.util.KvHelper;
+
+public class XpHelper {
+    @SuppressLint("StaticFieldLeak")
+    public static Context context;
+    public static ClassLoader classLoader;
+
+    public static String moduleApkPath;
+
+    public static void initContext(Context application) {
+        context = application;
+        classLoader = application.getClassLoader();
+        ClassUtils.intiClassLoader(classLoader);
+        KvHelper.initialize(application);
+        ActivityProxyManager.initActivityProxyManager(application);
+    }
+
+    public static void initZygote(IXposedHookZygoteInit.StartupParam startupParam) {
+        moduleApkPath = startupParam.modulePath;
+    }
+
+    /**
+     * 设置配置存储路径
+     */
+    public static void setConfigPath(String pathDir) {
+        KvHelper.initialize(pathDir);
+    }
+
+    /**
+     * 设置配置默认密码
+     * @param password 密码 采用AES加密 不设置则不使用加密
+     */
+    public static void setConfigPassword(String password) {
+        KvHelper.setGlobalPassword(password);
+    }
+
+    /**
+     * 注入模块的Res资源到上下文中
+     *
+     * @param context 要注入的上下文
+     */
+    public static void injectResourcesToContext(Context context) {
+        ActivityTools.injectResourcesToContext(context, moduleApkPath);
+    }
+
+}
